1d5eed31db277e922e82e158ff32fbb7f9042ac2
sanya <sanstuti@Sanstutis-MacBook-Pro.local>
2020-03-30 20:10:56 +0200

    Created and added backend spring project to backend branch.

---
+++ MancalaBackend/src/main/java/com/mancala/backend/MancalaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mancala.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MancalaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MancalaBackendApplication.class, args);
+	}
+
+}
---
+++ MancalaBackend/src/test/java/com/mancala/backend/MancalaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mancala.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MancalaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
